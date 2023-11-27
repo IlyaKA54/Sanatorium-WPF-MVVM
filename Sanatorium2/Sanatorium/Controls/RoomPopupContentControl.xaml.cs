a447a3f2ba6c8c781f2e298efb98473cbb3347ca
Ilya <132289743+IlyaKA54@users.noreply.github.com>
2023-11-28 00:08:18 +0300

    Fixed customer view

---
+++ Sanatorium2/Sanatorium/Controls/RoomPopupContentControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Sanatorium.Model.Entities;
+using System.Windows.Controls;
+
+namespace Sanatorium.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для RoomPopupContentControl.xaml
+    /// </summary>
+    public partial class RoomPopupContentControl : UserControl
+    {
+        public RoomPopupContentControl(Room room)
+        {
+            InitializeComponent();
+            DataContext = room;
+        }
+    }
+}
